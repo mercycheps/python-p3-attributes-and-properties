caf58ff390bb716730c6b7a47a026cfc8c15537f
Mercy <maritimmercy8@gmail.com>
2025-05-09 18:50:56 +0300

    added property

--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,43 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
+
 class Person:
-    pass
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC",
+        "Production", "Legal", "Finance"
+    ]
+
+    def __init__(self, name="Jane Doe", job="Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.", flush=True)
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
